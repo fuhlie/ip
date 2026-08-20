ea2c197f1f79d776b6f72ff4fe26bb394729e918
gt <no@email.com>
2026-08-20 15:06:26 +0000

    Echo user input until exit

--- src/main/java/Nori.java
+++ src/main/java/Nori.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Entry point for the Nori task-tracking chatbot.
  */
@@ -9,6 +11,17 @@ public class Nori {
         System.out.println("Hello! I'm Nori.");
         System.out.println("What can I do for you?");
         System.out.println(LINE);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+            System.out.println(LINE);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(LINE);
     }
